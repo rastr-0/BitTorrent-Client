f74008c73083ef5399cf619d512d288a388bb348
rastr <romanmilko123@gmail,com>
2024-01-23 01:46:17 +0100

    piece_manager object added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import utilities
 from torrent import Torrent
 from bcoding import bdecode
+from piece_manager import PieceManager
 
 
 # DONE: Implemented basic logic for Piece and PieceManager classes
@@ -27,13 +28,14 @@ if __name__ == '__main__':
     peers_list = decoded_response['peers']
 
     from peer_manager import PeerManager
+    # create PieceManager object
+    piece_manager = PieceManager(torrent_object)
 
     # create PeerManager object
-    manager = PeerManager(number_of_pieces)
+    manager = PeerManager(piece_manager, number_of_pieces)
     # connect to peers and exchange handshakes with peers
     manager.connect_to_peers(peers_list)
 
     manager.test_send_interested()
     # handling incoming messages from the connected peers
     manager.run()
-
