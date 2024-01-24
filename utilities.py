e64000159fc4866f10c3c316558c5c1c9501f677
rastr <romanmilko123@gmail,com>
2024-01-24 14:18:19 +0100

    generate_client_id function is deleted from utilities file and placed in torrent file

--- utilities.py
+++ utilities.py
@@ -1,19 +1,5 @@
 import logging
-from hashlib import sha1
-import random
-import string
 from torrent import Torrent
-
-
-def generate_client_id():
-    # my bittorrent client identifier
-    client_identifier = "RA1000"
-    # random 18 bytes string
-    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
-    peer_id_not_decoded = f"-{client_identifier}-{random_string}"
-    peer_id = sha1(peer_id_not_decoded.encode('utf-8')).digest()
-
-    return peer_id
 
 
 def get_pieces_number(torrent_file: Torrent):
